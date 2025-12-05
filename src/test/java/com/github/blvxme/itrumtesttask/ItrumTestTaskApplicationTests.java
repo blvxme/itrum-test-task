e19ad2cbded2a59cfab0d4a340b455e90e74adde
Mikhail <blvxme@gmail.com>
2025-12-05 12:46:02 +0700

    chore: initial commit

---
+++ src/test/java/com/github/blvxme/itrumtesttask/ItrumTestTaskApplicationTests.java
@@ -0,0 +1,10 @@
+package com.github.blvxme.itrumtesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItrumTestTaskApplicationTests {
+    @Test
+    void contextLoads() { }
+}
